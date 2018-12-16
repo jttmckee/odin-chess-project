d5ecfd414baa24f1bacbc4469abd31e4d1538fc6
jttmckee <wp.jason@hotmail.co.uk>
2018-12-16 17:34:30 +0000

    Use context to DRY up

--- spec/support/shared_examples/piece_core.rb
+++ spec/support/shared_examples/piece_core.rb
@@ -1,16 +1,17 @@
 RSpec.shared_examples "core Piece" do
-  it "False outside of board range (-x)" do
-    expect(subject.legal_move?(-1,1)).to be false
+  context "false when outside of board range" do
+    it  "(-x)" do
+      expect(subject.legal_move?(-1,1)).to be false
+    end
+    it "(-y)" do
+      expect(subject.legal_move?(1,-1)).to be false
+    end
+    it "(x=100)" do
+     expect(subject.legal_move?(100,1)).to be false
+    end
+    it "(y=100)" do
+      expect(subject.legal_move?(1,100)).to be false
+    end
   end
-  it "False outside of board range (-y)" do
-  expect(subject.legal_move?(1,-1)).to be false
-  end
-  it "False outside of board range (x=100)" do
-   expect(subject.legal_move?(100,1)).to be false
-  end
-  it "False outside of board range (y=100)" do
-  expect(subject.legal_move?(1,100)).to be false
-  end
-
 
 end
