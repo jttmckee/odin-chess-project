dc76a60bec8ffb5818b9ab2cefdaeecb8b4a1bda
jttmckee <wp.jason@hotmail.co.uk>
2018-12-17 16:22:03 +0000

    refactored board and position tests and filled some in

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,19 @@ class Board
     @range = range
   end
   def new_piece=(piece)
-
+    unless piece.x > 0 and piece.y > 0 and piece.x <= @range and piece.y <= @range
+      return false
+    else
+      @grid[piece.x,piece.y] = piece
+      return true
+    end
   end
+  def [](x,y)
+    unless x > 0 and y > 0 and x <= @range and y <= @range
+      return nil
+    else
+      return @grid[x,y]
+    end
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe "Board" do
       expect(board.new_piece= piece).to be false
       expect(board[1,1]).to eql(old_piece)
     end
+    it "fails if outside range" do
+      piece = Piece.new(9,9,:white,board)
+      expect(board.new_piece= piece).to be false
+    end
   end
   describe "[]" do
     it "pulls the piece at that location" do
@@ -19,6 +23,33 @@ RSpec.describe "Board" do
       board.new_piece= piece
       expect(board[1,1]).to eql(piece)
     end
+
+    it "returns nil if there is no piece" do
+      expect(board[7,7]).to be_nil
+    end
+    context "returns nil if outside range as" do
+      it  "x greater than 8" do
+        board.new_piece = Piece.new(9,7,:white,board)
+        expect(board[9,7]).to be_nil
+      end
+      it  "y greater than 8" do
+        board.new_piece = Piece.new(7,9,:white,board)
+        expect(board[7,9]).to be_nil
+      end
+      it  " x less than 1" do
+        board.new_piece = Piece.new(-1,2,:white,board)
+        expect(board[-1,2]).to be_nil
+      end
+      it  " y less than 1" do
+        board.new_piece = Piece.new(1,-2,:white,board)
+        expect(board[1,-2]).to be_nil
+      end
+      it  " equals zero" do
+        board.new_piece = Piece.new(0,0,:white,board)
+        expect(board[0,0]).to be_nil
+      end
+    end
+
 
   end
 
