68e82b0fd9fc8c49496d18b9f3bb27e19d76e769
jttmckee <wp.jason@hotmail.co.uk>
2018-12-23 15:40:42 +0000

    Correcting Knight move

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,7 +3,7 @@ require 'piece.rb'
 class Knight < Piece
   def legal_move?(new_x,new_y)
     diff_x, diff_y = diff(new_x,new_y)
-    legal = (diff_x == 1 && diff_y == 3) ||  (diff_x == 3 && diff_y == 1)
+    legal = (diff_x == 1 && diff_y == 2) ||  (diff_x == 2 && diff_y == 1)
     return legal && super(new_x,new_y)
   end
 
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -5,20 +5,19 @@ RSpec.describe Knight do
   subject(:knight){ Knight.new(:b,2,:black,Board.new) }
   describe "#legal_move?" do
     it "returns true if legal move" do
-      expect(knight.legal_move?(:c,5)).to be true
+      expect(knight.legal_move?(:c,4)).to be true
     end
     it "returns false if not permitted Knight move(y)" do
       expect(knight.legal_move?(:c,6)).to be false
     end
     it "returns false if not permitted Knight move(x)" do
-      expect(knight.legal_move?(:h,5)).to be false
+      expect(knight.legal_move?(:h,4)).to be false
     end
     #check inheriting from piece
     let(:new_piece) {new_piece = Piece.new(:c,5,:black,subject.board) }
     include_examples "allowed move"
   end
 
-  subject(:knight){ Knight.new(:b,2,:black,Board.new) }
   include_examples "set colour"
 
   include_examples "#new"
